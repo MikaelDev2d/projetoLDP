90d36ad8276fabe368130abf24102dbf496396eb
MikaelDev2d <Mikaelroblox076@gmail.com>
2025-05-31 10:43:57 -0300

    Update Projeto'Nome, Idade'[IF,ELSE]#1.py

--- Projeto'Nome, Idade'[IF,ELSE]#1.py
+++ Projeto'Nome, Idade'[IF,ELSE]#1.py
@@ -6,18 +6,3 @@ if int(idade) >= 18:
     print("Você é maior de idade!")
 else:
     print("você é de menor!")
-
-#primeiras informaçôes acima.
-
-quer_desafio = input("você quer um desafio? (s/n): ")
-
-#desafio aceito ou não abaixo.
-
-if quer_desafio == "s":
-    resposta = input("Qual é a montanha mais alta do planeta? ")
-    if resposta == "MonteEverest":
-      print("Você acertou!")
-    else:
-      print("Você errou!")
-if quer_desafio == "n":
-      print("talvez na próxima!")
